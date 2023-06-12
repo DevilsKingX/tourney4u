cb0384637e3507c41b8a2cc80ae03459a8874723
DevilsKingX <85121306+DevilsKingX@users.noreply.github.com>
2023-06-12 15:55:56 +0530

    sdgsdg

--- pages/api/tourneycreate.js
+++ pages/api/tourneycreate.js
@@ -2,5 +2,5 @@ import TourneyFunc from 'components/tourneyfunc.js'
 
 
 module.exports = async function (req, res) {
- return TourneyFunc(req, res)
+  return res.status(401).json({ error: 'Unauthorized' });
 };
