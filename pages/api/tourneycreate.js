72a5715ed55b3246517f5b18fd8be1cec8504084
DevilsKingX <85121306+DevilsKingX@users.noreply.github.com>
2023-11-01 16:13:19 +0530

    api

--- pages/api/tourneycreate.js
+++ pages/api/tourneycreate.js
@@ -53,6 +53,6 @@ module.exports = async function (req, res) {
     return res.status(200).send(buffer);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return res.status(500).json({ error: 'Internal Server Error:'+error });
   }
 };
